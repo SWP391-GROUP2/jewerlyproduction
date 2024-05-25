0d055d1b6b175c52a59e0c68141557dfaa5d2f44
ace2358 <150383774+ace2358@users.noreply.github.com>
2024-05-25 16:42:13 +0700

    Add files via upload
    tạm thời đăng, chưa chắc mấy thằng user, cần coi lại

---
+++ Entities/Staff.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JPOS_Bussiness_Objects
+{
+    internal class Staff : User
+    {
+        private string _role {  get; set; }
+        public Staff(string ID, string name, string dateOfBirth, string phoneNumber, string accountID, string username, string password, string email, string role) : base(ID, name, dateOfBirth, phoneNumber, accountID, username, password, email)
+        {
+            _role = role;
+        }
+    }
+}
