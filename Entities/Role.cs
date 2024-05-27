69ac40783e8aae989868808861c62420f98bf438
hungmegame123fpt <hungthinh26072004@gmail.com>
2024-05-28 00:43:33 +0700

    Add role entities

---
+++ Entities/Role.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    internal class Role
+    {
+        private int _roleID { get; set; }
+        private string _name { get; set; }
+        private string _description { get; set; }
+        private int _accessLevel { get; set; }
+
+        public Role(int roleID, string name, string description, int accessLevel)
+        {
+            _roleID = roleID;
+            _name = name;
+            _description = description;
+            _accessLevel = accessLevel;
+        }
+    }
+}
