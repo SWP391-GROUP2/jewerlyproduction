6f1842c6cb57a87fe5174610c256a48a9d95ce50
hungmegame123fpt <hungthinh26072004@gmail.com>
2024-05-28 19:17:43 +0700

    Them APi vo

---
+++ JewelryProduction.API/Model/LoginModel/LoginViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Core.Model
+using Microsoft.AspNetCore.Identity
+
+
+namespace JewelryProduction.API.Model.LoginModel
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "The Email field is required.")]
+        [EmailAddress(ErrorMessage = "The Email field is not a valid e-mail address.")]
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Mật khẩu")]
+        public string Password { get; set; }
+
+        [Display(Name = "Lưu thông tin tài khoản?")]
+        public bool RememberMe { get; set; }
+
+        [Display(Name = "Đăng nhập nhanh")]
+        public bool LoginFast { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
---
+++ JewelryProduction.API/Model/LoginModel/RegisterRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace JewelryProduction.API.Model.LoginModel
+{
+    public class RegisterRequest
+    {
+        public string name { get; set; }
+        public string _dateOfBirth { get; set; }
+        public string _phoneNumber { get; set; }
+        public string _email { get; set; }
+        public string _password { get; set; }
+        public string _confirmPassword { get; set; }
+        public bool EmailConfirmed { get; set; } 
+    }
+}
