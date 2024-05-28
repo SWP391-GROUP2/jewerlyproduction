4ddedad6ac01e9288b8412e6a9a9ef0da4c9b366
hungmegame123fpt <hungthinh26072004@gmail.com>
2024-05-29 01:39:16 +0700

    Add UserModel

---
+++ JewelryProduction.API/Model/UserModel/RequestCreateUserModel.cs
@@ -0,0 +1,14 @@
+﻿namespace JewelryProduction.API.Model.UserModel
+{
+    public class RequestCreateUserModel
+    {
+        public string? _id { get; set; }
+        public string? _name { get; set; }
+        public string? _dateOfBirth { get; set; }
+        public string? _phoneNumber { get; set; }
+        public string? _email { get; set; }
+        public string? _password { get; set; }
+        public int? _roleID { get; set; }
+    }
+}
+}
---
+++ JewelryProduction.API/Model/UserModel/RequestSearchUserModel.cs
@@ -0,0 +1,35 @@
+﻿namespace JewelryProduction.API.Model.UserModel
+{
+    public class RequestSearchUserModel
+    {
+        public string? _id { get; set; }
+        public string? _name { get; set; }
+        public string? _dateOfBirth { get; set; }
+        public string? _phoneNumber { get; set; }
+        public string? _email { get; set; }
+        public string? _password { get; set; }
+        public int? _roleID { get; set; }
+        public SortContent? SortContent { get; set; }
+    }
+    public class SortContent
+    {
+        public SortUserByEnum sortUserBy { get; set; }
+        public SortUserTypeEnum sortUserType { get; set; }
+    }
+    public enum SortUserByEnum
+    {
+        _id = 1,
+        _name = 2,
+        _dateOfBirth = 3,
+        _phoneNumber = 4,
+        _email = 5,
+        _roleID = 6,
+    }
+    public enum SortUserTypeEnum
+    {
+        Ascending = 1,
+        Descending = 2,
+    }
+}
+
+}
