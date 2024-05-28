386081dfcf68c764432a2ecf8a589339591cd3b6
hungmegame123fpt <hungthinh26072004@gmail.com>
2024-05-28 19:18:21 +0700

    Them class application

---
+++ JewelryProduction.Application/System/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JewelryProduction.API.Model;
+using Microsoft.AspNetCore.Identity.Data;
+namespace JewelryProduction.Application.System
+{
+    internal interface IUserService
+    {
+        Task<bool> authencate(API.Model.LoginRequest request);
+        Task<bool> register(RegisterRequest request);
+    }
+}
---
+++ JewelryProduction.Application/System/UserService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JewelryProduction.Application.System
+{
+    internal class UserService : IUserService
+    {
+    }
+}
