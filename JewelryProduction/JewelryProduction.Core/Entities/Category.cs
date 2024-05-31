8620456c696308baa14394afd0ee12762eb89634
ace2358 <150383774+ace2358@users.noreply.github.com>
2024-05-31 23:12:53 +0700

    Backend base files upload

---
+++ JewelryProduction/JewelryProduction.Core/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JewelryProduction;
+
+public partial class Category
+{
+    public string CategoryId { get; set; } = null!;
+
+    public string CategoryName { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public string GemstoneId { get; set; } = null!;
+
+    public virtual Gemstone Gemstone { get; set; } = null!;
+}
---
+++ JewelryProduction/JewelryProduction.Core/Entities/Insurance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JewelryProduction;
+
+public partial class Insurance
+{
+    public string InsuranceId { get; set; } = null!;
+
+    public DateOnly StartDate { get; set; }
+
+    public DateOnly EndDate { get; set; }
+
+    public string OrderId { get; set; } = null!;
+
+    public virtual Order Order { get; set; } = null!;
+}
