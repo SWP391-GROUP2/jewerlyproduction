25369eb27ee80cae56d73856bd1d35d5bc54a80c
ace2358 <150383774+ace2358@users.noreply.github.com>
2024-05-31 23:41:04 +0700

    Add files via upload

---
+++ JewelryProduction/JewelryProduction.Core/Entities/PaymentMethod.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JewelryProduction;
+
+public partial class PaymentMethod
+{
+    public string PaymentMethodId { get; set; } = null!;
+
+    public string PaymentMethodName { get; set; } = null!;
+
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+}
