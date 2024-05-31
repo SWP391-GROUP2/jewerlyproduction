b2771141f2563452fc3d73a9ed49cc9768178efc
ace2358 <manhntse184228@fpt.edu.vn>
2024-06-01 01:40:15 +0700

    insert

---
+++ JewelryProduction/JewelryProduction.Core/Interface/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JewelryProduction.Core.Interface
+{
+    public interface IUserRepository
+    {
+        public ICollection<User> GetUser();
+    }
+}
