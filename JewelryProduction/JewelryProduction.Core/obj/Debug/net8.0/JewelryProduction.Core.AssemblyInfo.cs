1842f21e00b6b7346dfb55435db0382efada2512
ace2358 <manhntse184228@fpt.edu.vn>
2024-06-01 13:25:14 +0700

    check

--- JewelryProduction/JewelryProduction.Core/obj/Debug/net8.0/JewelryProduction.Core.AssemblyInfo.cs
+++ JewelryProduction/JewelryProduction.Core/obj/Debug/net8.0/JewelryProduction.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JewelryProduction.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2d8ff5ffd9eabeeac4a4812a86396b691623e1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92aae3789b39ed45861924060df807722f5738de")]
 [assembly: System.Reflection.AssemblyProductAttribute("JewelryProduction.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JewelryProduction.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
