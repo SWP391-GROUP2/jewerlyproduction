5de354c8d7c6150148ca3e153178a836517476b4
ace2358 <manhntse184228@fpt.edu.vn>
2024-06-05 14:32:48 +0700

    Register update

---
+++ JewelryProduction/JewelryProduction.Core/DTO/LoginDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JewelryProduction.Core.DTO
+{
+    public class LoginDTO
+    {
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+}
