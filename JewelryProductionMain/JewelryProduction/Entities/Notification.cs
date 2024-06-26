8e663cdc39def64f8f15485b8024e48f6e7d7aee
hungmegame123fpt <hungthinh26072004@gmail.com>
2024-06-26 19:10:17 +0700

    .

--- JewelryProductionMain/JewelryProduction/Entities/Notification.cs
+++ JewelryProductionMain/JewelryProduction/Entities/Notification.cs
@@ -2,5 +2,10 @@
 {
     public class Notification
     {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Message { get; set; }
+        public bool IsRead { get; set; }
+        public DateTime CreatedAt { get; set; }
     }
 }
