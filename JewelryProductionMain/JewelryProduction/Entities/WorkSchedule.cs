33ad7d44022cc1f4c0e29f792b0d111971dda134
hungmegame123fpt <hungthinh26072004@gmail.com>
2024-06-30 23:37:38 +0700

    .

---
+++ JewelryProductionMain/JewelryProduction/Entities/WorkSchedule.cs
@@ -0,0 +1,12 @@
+﻿namespace JewelryProduction.Entities
+{
+    public class WorkSchedule
+    {
+        public string ScheduleId { get; set; }
+        public DateTime Date { get; set; }
+        public string UserId { get; set; }
+        public AppUser User { get; set; }
+
+        public int RequiredEmployees { get; set; }
+    }
+}
