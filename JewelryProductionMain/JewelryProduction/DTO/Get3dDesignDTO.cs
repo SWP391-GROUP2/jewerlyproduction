b35a2b80e91f211d9da261c7d0961d92304ed797
ace2358 <manhntse184228@fpt.edu.vn>
2024-07-08 08:19:46 +0700

    3d Design BE

---
+++ JewelryProductionMain/JewelryProduction/DTO/Get3dDesignDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace JewelryProduction.DTO
+{
+    public class Get3dDesignDTO
+    {
+        public string _3dDesignId { get; set; }
+
+        public string DesignName { get; set; } = null!;
+
+        public string Image { get; set; }
+
+        public string? CustomizeRequestId { get; set; }
+
+        public string? ProductSampleId { get; set; }
+
+        public string DesignStaffId { get; set; } = null!;
+    }
+}
