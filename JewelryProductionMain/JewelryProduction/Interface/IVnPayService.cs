00f8cc0fd9dc85619abbd34c6e6ac28183e1dd90
hiTech <braykul2605@gmail.com>
2024-07-08 12:21:33 +0700

    Fixed again payment

--- JewelryProductionMain/JewelryProduction/Interface/IVnPayService.cs
+++ JewelryProductionMain/JewelryProduction/Interface/IVnPayService.cs
@@ -4,7 +4,7 @@ namespace JewelryProduction.Interface
 {
     public interface IVnPayService
     {
-        Task<string> CreatePaymentUrl(HttpContext context, VnPaymentRequestModel model);
+        public Task<string> CreatePaymentUrl(HttpContext context, double price, string orderID);
         VnPaymentResponseModel PaymentExecute(IQueryCollection collection);
     }
 }
