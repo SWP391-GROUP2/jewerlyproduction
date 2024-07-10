7d078b8d67c0bac5df17657193bb31e9b8b60a79
hiTech <braykul2605@gmail.com>
2024-07-10 14:54:42 +0700

    UPDATE GETUSERS

---
+++ JewelryProductionMain/JewelryProduction/DTO/UserWithRoleDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace JewelryProduction.DTO
+{
+    public class UserWithRoleDTO
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public IList<string> Roles { get; set; }
+    }
+}
