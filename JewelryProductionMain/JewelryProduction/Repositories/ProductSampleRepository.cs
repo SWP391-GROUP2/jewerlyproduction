8738d0a52d7144adf40b08466cd2606e71afdc6a
ace2358 <manhntse184228@fpt.edu.vn>
2024-07-20 03:21:22 +0700

    fix get samples

--- JewelryProductionMain/JewelryProduction/Repositories/ProductSampleRepository.cs
+++ JewelryProductionMain/JewelryProduction/Repositories/ProductSampleRepository.cs
@@ -92,6 +92,8 @@ namespace JewelryProduction.Repositories
         {
             var samples = await _context.ProductSamples
                 .Include(d => d.Gold)
+                .Include(d => d._3ddesigns)
+                .Include(d => d.Gemstones)
                 .ToListAsync();
 
             var result = samples.Select(samples => new GetProductSampleDTO
@@ -106,6 +108,8 @@ namespace JewelryProduction.Repositories
                 GoldId = samples.GoldId,
                 GoldType = samples.Gold.GoldType,
                 GoldWeight = samples.GoldWeight,
+                GemstoneId = samples.Gemstones?.Select(g => g.GemstoneId).ToList(),
+                _3dDesignId = samples._3ddesigns?.Select(ds => ds.ProductSampleId).ToList(),
             }).ToList();
 
             return result;
